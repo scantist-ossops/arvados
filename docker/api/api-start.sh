99596692f966abb345498bd4b0e20c3a0dcbfb98
Peter Amstutz <peter.amstutz@curii.com>
2023-02-28 18:40:06 -0500

    16385: Dockerized API+Controller WIP
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

---
+++ docker/api/api-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (C) The Arvados Authors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Start the first process
+(cd /var/www/arvados-api/current && passenger start) &
+
+# Start the second process
+arvados-controller &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
